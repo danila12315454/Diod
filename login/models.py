3fce3f24191d7d7f3e8fdf41ebacb5c4a89e0805
DIOD <danilaprig@gmail.com>
2021-08-06 15:16:21 +0300

    test

--- login/models.py
+++ login/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
-class Main(models.Model):
+"""class Main(models.Model):
     login = models.CharField()
     password = models.CharField()
     name = models.CharField()
     nik = models.CharField()
     photo = models.ImageField(upload_to="photos/avatars")
     time_create = models.DateTimeField
+    rtyuio"""
